8cf67c0b71fe538b573888083596d447721b1927
minhhau2423 <71492981+minhhau2423@users.noreply.github.com>
2022-06-19 16:22:16 +0700

    up

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
+from email.mime import image
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
 from flask import jsonify
-
-
+import cv2
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
-class Test(Resource):
-    def get(self):
-        return "TEST response"
-
-@app.route("/", methods=["GET"])
+@app.route("/")
 def Welcome():
     return "<h1>Welcome to digital image processing API</h1>"
+
+class Test(Resource):
+    def get(self):
+        absolute_path = os.path.join(os.getcwd(), 'datascient', 'Image', 'img.jpg')
+        return absolute_path
 
 
 api.add_resource(Test, '/test') 
 
 
 if __name__ == '__main__':
-     app.run()
+    absolute_path = os.path.join(os.getcwd(), 'datascient', 'Image', 'img.jpg')
+   
+    img = cv2.imread(absolute_path,1)
+    cv2.imshow(img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    path = 'E:\_TRUONGMINHHAU\DataScience\Demo_DIP\datascient\Image'
+    cv2.imwrite(os.path.join(path , 'waka.jpg'), gray)
+    cv2.waitKey(0)
+    app.run()
