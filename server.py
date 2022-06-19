681c60ac2d606ec272cc8af80d7a8ecfe3f10983
minhhau2423 <71492981+minhhau2423@users.noreply.github.com>
2022-06-19 12:59:21 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from turtle import home
 from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
 from json import dumps
 from flask import jsonify
 
@@ -11,7 +10,7 @@ api = Api(app)
 
 class Test(Resource):
     def get(self):
-        return "tran anh vu du"
+        return "TEST response"
 class Welcome(Resource):
     def get(self):
         return "Python API"
