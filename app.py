28466aff2152ca262a3509fe8eef2405302c38e1
minhhau2423 <71492981+minhhau2423@users.noreply.github.com>
2022-06-19 17:16:36 +0700

    t

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from email.mime import image
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
@@ -11,24 +10,20 @@ api = Api(app)
 
 @app.route("/")
 def Welcome():
-    return "<h1>Welcome to digital image processing API</h1>"
+    return "<h1>Welcome to digital image processing API ver 2 </h1>"
 
 class Test(Resource):
-    def get(self):
+    def get(self): 
         absolute_path = os.path.join(os.getcwd(), 'datascient', 'Image', 'img.jpg')
-        return os.path.join(os.getcwd())
+        img = cv2.imread(absolute_path,1)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return  "<image source={require("+absolute_path+")}/>"  
 
 
 api.add_resource(Test, '/test') 
-
-
 if __name__ == '__main__':
     absolute_path = os.path.join(os.getcwd(), 'datascient', 'Image', 'img.jpg')
-   
-    img = cv2.imread(absolute_path,1)
-    
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    path = 'E:\_TRUONGMINHHAU\DataScience\Demo_DIP\datascient\Image'
-    cv2.imwrite(os.path.join(path , 'waka.jpg'), gray)
-    cv2.waitKey(0)
+    print(absolute_path)
+    # img = cv2.imread(absolute_path,1)
+    # cv2.imshow('image',img)
     app.run()
